5459c7819c758bde3e90b656dd404ca9a8ce7d53
Chatano <caepradiee@gmail.com>
2024-07-06 02:32:49 -0300

    refactor: remove motion component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,12 @@
-import { MotionImage } from './motion/image'
+'use client'
+import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 interface Props {
   size?: number
 }
+
+const MotionImage = motion(Image)
 
 export function Logo({ size = 160 }: Props) {
   return (
--- src/components/motion/image.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-import Image from 'next/image'
-import { motion } from 'framer-motion'
-
-export const MotionImage = motion(Image)
